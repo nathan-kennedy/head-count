a41ab730d414888b6aa989815d0462cfaac82584
nathan-kennedy <nathankennedy1@gmail.com>
2022-07-05 00:19:13 -0400

    head count app finished

---
+++ index.js
@@ -0,0 +1,36 @@
+let count = 0;
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+let storedCount = 0;
+
+
+function increment () {
+    count += 1;
+    document.getElementById("count-el").textContent = count;
+}
+
+function decrement() {
+    count -= 1;
+    document.getElementById("count-el").textContent = count;
+}
+
+function save() {
+    if (storedCount === 0) {
+        storedCount = count;
+        saveEl.textContent = "TOTAL: " + storedCount;
+    } else {
+        storedCount += count;
+        saveEl.textContent = "TOTAL: " + storedCount;
+    }
+    count = 0;
+    countEl.textContent = count;
+}
+
+function reset() {
+    count = 0;
+    storedCount = 0;
+    countEl.textContent = count;
+    saveEl.textContent = "TOTAL: 0"
+}
+
+    
